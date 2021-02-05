23c3ff2e446003aafbed5b83097518ace732661a
cidmoosa123 <77245783+cidmoosa123@users.noreply.github.com>
2021-02-05 11:50:24 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,12 +4,13 @@ user=$1vcpb #Make change here if you changed username
 exec su "$user" "$0" -- "$@"
 fi
 echo "This will be run from user $UID"
-cp -f /home/__init__.py /home/vcpb/config/__init__.py
+cp -f /home/__init__.py /home/vcbot/config/__init__.py
 mv /home/Telegram /home/vcpb/Telegram #Move Telegram to VCPB's Home (make changes according to username)
 pactl load-module module-null-sink sink_name=MySink #loading MySink
+pactl load-module module-remap-source master=MySink.monitor source_name=virtual_microphone source_properties=device.description=VoiceChatPyroBot
 echo "Pulseaudio MySink Loaded"
 pactl set-default-sink MySink #setting Mysink as default Mic
 echo "MySink Is Now Your Default Mic"
-echo "Now Start xrdp and login and join a VoiceChat"
-cd /home/vcpb 
+echo "Now Start vnc and login and join a VoiceChat"
+cd /home/vcbot 
 python3 bot.py
